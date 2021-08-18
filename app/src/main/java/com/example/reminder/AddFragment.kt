56258b41979bcf5037e14a22f094f64014cbf4af
DipeshSingh1772 <dipeshsingh1772@gmail.com>
2021-08-18 23:37:02 +0530

    view data binding added

--- app/src/main/java/com/example/reminder/AddFragment.kt
+++ app/src/main/java/com/example/reminder/AddFragment.kt
@@ -5,15 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.reminder.databinding.FragmentAddBinding
 
 class AddFragment : Fragment() {
+
+    private var _binding:FragmentAddBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        _binding = FragmentAddBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
 }
--- app/src/main/java/com/example/reminder/EditFragment.kt
+++ app/src/main/java/com/example/reminder/EditFragment.kt
@@ -5,14 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.reminder.databinding.FragmentEditBinding
 
 class EditFragment : Fragment() {
+
+    private var _binding:FragmentEditBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit, container, false)
+       _binding = FragmentEditBinding.inflate(inflater,container,false)
+
+        return binding.root
     }
 }
