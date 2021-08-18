efc436859b5a15a18e356ae97431b0f125eb88a1
DipeshSingh1772 <dipeshsingh1772@gmail.com>
2021-08-19 00:02:18 +0530

    icon added

--- app/src/main/java/com/example/reminder/AddFragment.kt
+++ app/src/main/java/com/example/reminder/AddFragment.kt
@@ -15,7 +15,7 @@ class AddFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         _binding = FragmentAddBinding.inflate(inflater, container, false)
 
--- app/src/main/java/com/example/reminder/MainFragment.kt
+++ app/src/main/java/com/example/reminder/MainFragment.kt
@@ -15,7 +15,7 @@ class MainFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         _binding = FragmentMainBinding.inflate(inflater, container, false)
 
