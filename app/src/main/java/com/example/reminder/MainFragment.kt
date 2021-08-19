e059c632a7308605bffdf6013dc14e9a9faa1ca7
DipeshSingh1772 <dipeshsingh1772@gmail.com>
2021-08-19 23:05:54 +0530

    memo add page desing added

--- app/src/main/java/com/example/reminder/MainFragment.kt
+++ app/src/main/java/com/example/reminder/MainFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.reminder.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
@@ -20,6 +21,16 @@ class MainFragment : Fragment() {
         _binding = FragmentMainBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        _binding?.addBtn?.setOnClickListener {
+
+            findNavController().navigate(R.id.action_mainFragment_to_addFragment)
+        }
+
     }
 
 }
