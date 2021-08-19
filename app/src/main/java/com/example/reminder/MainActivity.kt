9ab50824fd2b5ed8dd2209fafefa2496d9e8b89d
DipeshSingh1772 <dipeshsingh1772@gmail.com>
2021-08-19 21:37:31 +0530

    home page desing added

--- app/src/main/java/com/example/reminder/MainActivity.kt
+++ app/src/main/java/com/example/reminder/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.reminder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar?.hide()
+
         setContentView(R.layout.activity_main)
     }
 }
