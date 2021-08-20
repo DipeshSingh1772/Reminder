4754b10d5672226518145656f9a79c0b5c539eac
DipeshSingh1772 <dipeshsingh1772@gmail.com>
2021-08-21 01:35:08 +0530

    Date Picker Added

--- app/src/main/java/com/example/reminder/AddFragment.kt
+++ app/src/main/java/com/example/reminder/AddFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.reminder.database.Memo
@@ -97,6 +98,9 @@ class AddFragment : Fragment() {
         if(item.isNotEmpty()) {
             viewModel.insertMemo(Memo(0,item,0))
             findNavController().navigate(R.id.action_addFragment_to_mainFragment)
+        }
+        else{
+            Toast.makeText(this.requireContext(), "Field Empty", Toast.LENGTH_SHORT).show()
         }
     }
 
